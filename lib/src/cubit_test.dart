61455f003ebbe12912a4be88ec4af5aa3b18b5b3
KihaJeong1994 <jkha7371@gmai.com>
2023-03-02 17:52:53 +0900

    stream subscribe & onChange

--- lib/src/cubit_test.dart
+++ lib/src/cubit_test.dart
@@ -1,16 +1,30 @@
 import 'package:bloc/bloc.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
   final cubit = CounterCubit(0);
   print(cubit.state);
+  final subscription = cubit.stream.listen(print);
+  await Future.delayed(Duration(seconds: 3));
   cubit.increment();
-  print(cubit.state);
-  cubit.close();
+  await Future.delayed(Duration.zero);
+  await subscription.cancel();
+  await cubit.close();
   // cubit.increment(); //Bad state: Cannot emit new states after calling close
   // print(cubit.state);
+  print('---------');
+  CounterCubit(0)
+    ..increment()
+    ..close();
 }
 
 class CounterCubit extends Cubit<int> {
   CounterCubit(super.initialState);
+
   void increment() => emit(state + 1);
+
+  @override
+  void onChange(Change<int> change) {
+    super.onChange(change);
+    print(change);
+  }
 }
